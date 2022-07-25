5ecd8c0654ad012dadb7e78cca5628080ac145cf
merhawi <mwoldemichael>
2022-07-24 22:36:19 -0500

    indexupdate

--- tipcalculator/tip.js
+++ tipcalculator/tip.js
@@ -1,14 +1,15 @@
 //A function that calculate tip and give the final total price
 function calculateTip() {
     //getting the subtotal
+    //100
     let subtotal = document.getElementById("subTotal").value;
     //getting percentage
     let percentage = document.getElementById("percentage").value;
-    
+    //25
     //parsing the value to number
     let total = parseInt(subtotal);
     let percent = parseInt(percentage);
-
+  
     document.getElementById("total").innerHTML = (total * (percent / 100)) + parseInt(subtotal);
 }
 
